e442ef7d2a81e20d3d5447c3ec3df4f802c0a13c
Fares <fares@Faress-Laptop.local>
2024-06-08 10:07:51 +0100

    removing dev mode

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,8 @@ export default async function Home() {
           const { data, error } = await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
-              redirectTo: `${process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://internal-booking-system.vercel.app"}/api/auth/callback`,
+              redirectTo:
+                "https://internal-booking-system.vercel.app/api/auth/callback",
               queryParams: {
                 next: "bookings",
               },
